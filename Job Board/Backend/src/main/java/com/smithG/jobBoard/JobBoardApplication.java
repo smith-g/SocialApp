4a1f5b15cfac47b03f5c0f11c36f424a89c0d046
smithgarcia <$Hazam!-013>
2022-12-05 21:10:24 -0800

    inital commit

---
+++ Job Board/Backend/src/main/java/com/smithG/jobBoard/JobBoardApplication.java
@@ -0,0 +1,13 @@
+package com.smithG.jobBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBoardApplication.class, args);
+	}
+
+}
